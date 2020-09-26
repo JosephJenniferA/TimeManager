45f6c00a9c009eeef4a784fee9be21d78f27bef9
Jennnifer Joseph <71746001+JosephJenniferA@users.noreply.github.com>
2020-09-25 22:37:14 -0700

    Update timecode.py

--- timecode.py
+++ timecode.py
@@ -1,11 +1,13 @@
 #create a program that can calculate how long I should spend doing tasks in each respective category
 print('What is your name?')
 name = input()  # read a single line and store it in the variable "name"
-print('Hi ' + name + '!')
-print('Ok ' + name + ' how many hours do you plan to spend daily to become a python developer?')
+
+print('Hi ' + name + ' how many hours do you plan to spend daily to become a python developer?')
+
 daily = input()  # read a single line and store it in the variable "daily"
 
 hoursdaily = input('Great, so you plan to spend ' + daily + ' hours to become a python developer. Is that right ' + name + '?')
+
 if hoursdaily.lower() == ('yes'):
     print("great to hear," + name + '!')
 else:
@@ -15,19 +17,20 @@ else:
 
 print("ok great. that means you will spend " + str(float(daily) * 60) + " minutes coding")
 
+total_minutes = float(daily) * 60
+print(total_minutes)
+
 print('what percentage of time should be spent actually coding?')
 
 coding_time = input()  # read a single line and store it in the variable "coding_time"
 
-print('Ok great, so you plan to spend ' + str(float(coding_time)) + "% of the time coding. That's great " + name + '!')
+print('Ok great, so you plan to spend ' + str(float(coding_time)) + "% of the time coding. That's great ")
 
-#determine the percent of time spent coding
+#correct percentage format!!!VVV
+print("%.0f%%" % (float(coding_time)))
 
-a_number = float(coding_time)*100
-percentage = "{:.0%}".format(a_number)
-print(percentage)
+#correct decimal format!!!!VVVV
+print(float(coding_time)/100)
 
-a_number = int(coding_time)/100
-print(float(daily) * 60)*str.format(float(coding_time))
-print((float(coding_time))/(float(daily) * 60))
-print(float(coding_time))
+print(float(total_minutes))
+print(float(total_minutes / "%.0f%%" % (float(coding_time))))
